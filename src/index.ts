5b369b5de322357866c4664f322b9424494d27ce
Tissyane <tissyane@gmail.com>
2022-11-23 12:13:20 -0300

    setup: initial project setup - installed dependencies, configured typescript,  created build and run scripts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
 dotenv.config();
 
 const server = express();
+server.use(cors());
 server.use(express.json());
 
 server.get("/status", (req, res) => {
